2902fac600cade8a13355d77ef70a27313d9f87a
Marko <110738528+trnjak@users.noreply.github.com>
2024-08-17 21:45:09 +0200

    Added new links and changed some text.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function AnchorFour(){
-    var lmao = ["https://www.youtube.com/watch?v=XDQLQpEAP3w", 
-                "https://www.youtube.com/watch?v=Efmq_uXt1Rk", 
-                "https://www.youtube.com/watch?v=NCtzkaL2t_Y",
-                "https://www.youtube.com/watch?v=TMssLuZOrE0",
-                "https://www.youtube.com/watch?v=dzANcb-VXd0",
-                "https://www.youtube.com/watch?v=vWW2SzoAXMo",
-                "https://www.youtube.com/watch?v=sepcj45774I"]
-    var random = Math.floor(Math.random() * 7)
+    var lmao = ["https://www.youtube.com/watch?v=XDQLQpEAP3w", //wind waker
+                "https://www.youtube.com/watch?v=Efmq_uXt1Rk", //welcome to hell
+                "https://www.youtube.com/watch?v=NCtzkaL2t_Y", //don't let me down
+                "https://www.youtube.com/watch?v=TMssLuZOrE0", //john hell
+                "https://www.youtube.com/watch?v=dzANcb-VXd0", //get off the road
+                "https://www.youtube.com/watch?v=vWW2SzoAXMo", //helter skelter
+                "https://www.youtube.com/watch?v=sepcj45774I", //only acting
+                "https://www.youtube.com/watch?v=lCv_XBTRVC0", //i
+                "https://www.youtube.com/watch?v=lxj5bzgFoGI"]; //take off your dress
+    var random = Math.floor(Math.random() * 9);
     var anchor4 = document.getElementById("anchor4");
-    anchor4.href = lmao[random]
+    anchor4.href = lmao[random];
 }
 
 function Tito(){
